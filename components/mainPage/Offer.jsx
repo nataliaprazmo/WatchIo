709cc712895100f06e7839da40846b6e70e059f3
nataliaprazmo <prazmonatalia@gmail.com>
2023-10-17 19:36:20 +0200

    offer page

--- components/mainPage/Offer.jsx
+++ components/mainPage/Offer.jsx
@@ -1,24 +1,60 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Nav from "./Nav";
 import Footer from "./Footer";
 import OfferService from "./OfferService";
 import { offer } from "@/data";
 
+import TvRoundedIcon from "@mui/icons-material/TvRounded";
+import LightRoundedIcon from "@mui/icons-material/LightRounded";
+import AddToQueueRoundedIcon from "@mui/icons-material/AddToQueueRounded";
+
 const Offer = () => {
+	const [activeOffer, setOffer] = useState(0);
 	return (
 		<div>
 			<Nav />
-			<div className="py-18">
-				{offer.map((service, id) => (
-					<OfferService
-						key={id}
-						id={service.id}
-						title={service.title}
-						description={service.description}
-						image={service.image}
-						tutorial={service.tutorial}
-					/>
-				))}
+			<div className="flex flex-wrap md:flex-row flex-col gap-[40px] items-center justify-center pt-12 mt-[80px]">
+				<div
+					className="flex flex-col items-center gap-4 border-2 border-white rounded-lg px-8 py-12 cursor-pointer active:border-primary-orange hover:border-primary-orange hover:px-10 transition-all duration-500"
+					onClick={() => setOffer(0)}
+				>
+					<LightRoundedIcon className="text-[48px] mb-4" />
+					<div className="md:w-[300px] w-[235px] h-[1.5px] bg-white" />
+					<h1 className="md:text-[24px] text-[20px] font-semibold text-primary-orange">
+						{offer[0].title}
+					</h1>
+				</div>
+				<div
+					className="flex flex-col items-center gap-4 border-2 border-white rounded-lg px-8 py-12 cursor-pointer active:border-primary-orange hover:border-primary-orange hover:px-10 transition-all duration-500"
+					onClick={() => setOffer(1)}
+				>
+					<TvRoundedIcon className="text-[48px] mb-4" />
+					<div className="md:w-[300px] w-[235px] h-[1.5px] bg-white" />
+					<h1 className="md:text-[24px] text-[20px] font-semibold text-primary-orange">
+						{offer[1].title}
+					</h1>
+				</div>
+				<div
+					className="flex flex-col items-center gap-4 border-2 border-white rounded-lg px-8 py-12 cursor-pointer active:border-primary-orange hover:border-primary-orange hover:px-10 transition-all duration-500"
+					onClick={() => setOffer(2)}
+				>
+					<AddToQueueRoundedIcon className="text-[48px] mb-4" />
+					<div className="md:w-[300px] w-[235px] h-[1.5px] bg-white" />
+					<h1 className="md:text-[24px] text-[20px] font-semibold text-primary-orange">
+						{offer[2].title}
+					</h1>
+				</div>
+			</div>
+			<div className="pb-18 pt-8 ">
+				<OfferService
+					id={offer[activeOffer].id}
+					title={offer[activeOffer].title}
+					description={offer[activeOffer].description}
+					image={offer[activeOffer].image}
+					tutorial={offer[activeOffer].tutorial}
+				/>
 			</div>
 			<Footer />
 		</div>
