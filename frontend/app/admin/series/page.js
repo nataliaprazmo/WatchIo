45a91bbb51561d8a106259a3d493f0b6741e4c21
nataliaprazmo <prazmonatalia@gmail.com>
2023-11-16 16:56:47 +0100

    small fixes

--- frontend/app/admin/series/page.js
+++ frontend/app/admin/series/page.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { NavMenu, FilmTable } from "@/components/admin";
+import { FilmTable } from "@/components/admin";
 import SiteBreadcrumbs from "@/components/SiteBreadcrumbs";
 
 export default function Page() {
 	return (
 		<main className="overflow-hidden pt-24 pb-8 pl-24 pr-10">
-			<NavMenu />
 			<SiteBreadcrumbs
 				links={[
 					{ label: "Panel", to: "/admin" },
