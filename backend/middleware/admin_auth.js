3a5feef7157a9c60c5047e9b59a0fcff49695bcb
radkam2000 <radkam2000@protonmail.com>
2023-11-20 18:27:09 +0100

    fix for admin auth

--- backend/middleware/admin_auth.js
+++ backend/middleware/admin_auth.js
@@ -1,6 +1,7 @@
 const { User } = require("../models/User");
-const admin_auth = (req, res, next) => {
-	const user = User.findById(req.user._id);
+const admin_auth = async (req, res, next) => {
+	const user = await User.findById(req.user._id);
+	console.log(user);
 	if (!user) return res.status(401).send({ message: "Unauthorized!" });
 	if (user.is_admin != true)
 		return res.status(401).send({ message: "Unauthorized!" });
