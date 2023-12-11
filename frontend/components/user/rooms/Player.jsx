ee6ac38b96054729387b063553c5852af4b98b31
nataliaprazmo <prazmonatalia@gmail.com>
2023-12-11 16:18:15 +0100

    sharing watching with web socket changes

--- frontend/components/user/rooms/Player.jsx
+++ frontend/components/user/rooms/Player.jsx
@@ -1,17 +1,18 @@
 "use client";
 import React, { useRef, useState } from "react";
-import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
-import socket from "./socket";
+import { ws } from "./socket";
 
 const Player = ({ id }) => {
 	const video = useRef(document.getElementById("video"));
-	const [watching, setWatching] = useState(null);
+	const [watching, setWatching] = useState(1);
+	const HEARTBEAT_TIMEOUT = 1000 * 5 + 1000 * 1; // 5 + 1 second
+	const HEARTBEAT_VALUE = 1;
 
 	const sendEvent = (eventType) => (e) => {
 		e.preventDefault();
-		if (socket) {
-			socket.send([eventType, e.target.currentTime]);
+		if (ws) {
+			ws.send([eventType, e.target.currentTime]);
 		}
 	};
 	const sendError = (e) => {
@@ -27,10 +28,42 @@ const Player = ({ id }) => {
 		console.log("right mouse clicked");
 	};
 
-	socket.addEventListener("open", function (event) {
+	// function closeConnection() {
+	// 	if (!!ws) {
+	// 		ws.close();
+	// 	}
+	// }
+
+	function heartbeat() {
+		if (!ws) {
+			return;
+		} else if (!!ws.pingTimeout) {
+			clearTimeout(ws.pingTimeout);
+		}
+
+		ws.pingTimeout = setTimeout(() => {
+			ws.close();
+
+			// business logic for deciding whether or not to reconnect
+		}, HEARTBEAT_TIMEOUT);
+
+		const data = new Uint8Array(1);
+
+		data[0] = HEARTBEAT_VALUE;
+
+		ws.send(data);
+	}
+
+	ws.addEventListener("open", function (event) {
 		console.log("Connected to WS Server");
 	});
-	socket.addEventListener("message", function (event) {
+	ws.addEventListener("message", function (event) {
+		console.log(typeof event.data);
+		if ("string" != typeof event.data) {
+			console.log(heartbeat);
+			heartbeat();
+			return;
+		}
 		let arrData = event.data.split(",");
 		// console.log(arrData[1] - video.current.currentTime);
 
@@ -42,13 +75,8 @@ const Player = ({ id }) => {
 			video.current.pause();
 		}
 		if (arrData[0] === "seeked") {
-			if (
-				arrData[1] - video.current.currentTime > 2 ||
-				arrData[1] - video.current.currentTime < -2
-			) {
-				video.current.currentTime = parseFloat(arrData[1]);
-				video.current.play();
-			}
+			video.current.currentTime = parseFloat(arrData[1]);
+			video.current.play();
 		}
 		if (arrData[0] === "watching") {
 			setWatching(arrData[1]);
--- frontend/components/user/rooms/socket.js
+++ frontend/components/user/rooms/socket.js
@@ -1,2 +1,4 @@
-const socket = new WebSocket("ws://localhost:5000");
-export default socket;
+const roomId = Date.now() + "" + Math.round(Math.random() * 1e9);
+console.log(roomId);
+const ws = new WebSocket("ws://localhost:5000/api/rooms/" + 1);
+export default { ws, roomId };
